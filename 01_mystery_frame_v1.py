57cfc7c1373b51138c251c09622e2522b7a4e588
Allenr71291 <52386917+Allenr71291@users.noreply.github.com>
2020-04-23 12:13:13 +1200

    mystery box v.2

--- 01_mystery_frame_v1.py
+++ 01_mystery_frame_v1.py
@@ -20,11 +20,11 @@ class Start:
         self.start_amount_entry = Entry(self.start_frame, font="Arial 16 bold")
         self.start_amount_entry.grid(row=1)
 
-        self.lowstakes_button = Button(self.to_game,
+        self.lowstakes_button = Button(self.start_frame,
                                        text="Low($5)",
                                        padx=10,
                                        pady=10,
-                                       command=self.to_game)
+                                       command=lambda: self.to_game(1))
         self.lowstakes_button.grid(row=2)
 
     def to_game(self, stakes):
@@ -38,6 +38,44 @@ class Game:
         print(starting_balance)
 
         partner.lowstakes_button.config(state=DISABLED)
+
+        self.balance = IntVar()
+
+        self.balance.set(starting_balance)
+
+        self.game_box = Toplevel()
+        self.game_frame = Frame(self.game_box)
+        self.game_frame.grid()
+
+        #Heading Row
+        self.heading_label = Label(self.game_frame, text="Heading",
+                                   font="Arial 24 bold", padx=10,
+                                   pady=10)
+        self.heading_label.grid(row=0)
+
+        #Balance Label
+        self.balance_frame = Frame(self.game_frame)
+        self.balance_frame.grid(row=1)
+
+        self.balance_label = Label(self.game_frame, text="Balance...")
+        self.balance_label.grid(row=3)
+
+        self.play_button = Button(self.game_frame, text="Gain",
+                                   padx=10, pady=10, command=self.reveal_boxes)
+        self.play_button.grid(row=4)
+
+    def reveal_boxes(self):
+        # retrieval the balance from the initial function
+        current_balance = self.balance.get()
+
+        #adjust balance
+        current_balance +=2
+
+        #set balance to new balance
+        self.balance.set(current_balance)
+
+        #Edit label so user can see balance
+        self.balance_label.configure(text="Balance: {}".format(current_balance))
 
 
 # main routine
---
+++ 02_Start_GUI_v1.py
@@ -0,0 +1,82 @@
+from tkinter import *
+from functools import partial
+
+
+class Start:
+    def __init__(self, parent):
+
+        background_color = "white"
+
+        self.start_frame = Frame(bg=background_color, pady=10, padx=10)
+        self.start_frame.grid()
+
+        self.temp_Converter_label = Label(self.start_frame,
+                                          text="Mystery Box Game",
+                                          font=("Arial", "19", "bold"),
+                                          bg=background_color,
+                                          padx=10, pady=10)
+        self.temp_Converter_label.grid(row=0)
+
+        self.mystery_instructions = Label(self.start_frame, font="Arial 10 italic",
+                                          text="please enter a dollar amount "
+                                          "(Between $5 and $50). in the box "
+                                          "below. Then choose the stakes."
+                                          "The higher the stakes the more "
+                                          "you can win!",
+                                          wrap=275, justify=LEFT, padx=10, pady=10)
+        self.mystery_instructions.grid(row=1)
+
+        self.start_amount_entry = Entry(self.start_frame, font="Arial 16 bold")
+        self.start_amount_entry.grid(row=2)
+
+        self.lowstakes_button = Button(self.start_frame,
+                                       text="Low($5)",
+                                       padx=10,
+                                       pady=10,
+                                       command=lambda: self.to_game(1))
+        self.lowstakes_button.grid(row=3)
+
+        # Entry box (row 2)
+        self.start_amount_entry = Entry(self.start_frame, font="Arial 19 bold")
+        self.start_amount_entry.grid(row=2)
+
+        # button frame (row 3)
+        self.stakes_frame = Frame(self.start_frame)
+        self.stakes_frame(row=3)
+
+        # Buttons here:
+        button_font = "arial 12 bold"
+        # Orange low stakes button
+        self.lowstakes_button = Button(self.stakes_frame, text="Low ($5)",
+                                       command=lambda: self.to_game(1),
+                                       font=button_font, bg="FF9933")
+        self.lowstakes_button.grid(row=0, column=0, pady=10)
+
+        #yellow medium stakes button
+        self.lowstakes_button = Button(self.stakes_frame, text="Medium ($10)",
+                                       command=lambda: self.to_game(2),
+                                       font=button_font, bg="FFFF33")
+        self.lowstakes_button.grid(row=0, column=0, pady=10)
+
+        self.lowstakes_button = Button(self.stakes_frame, text="High ($15)",
+                                       command=lambda: self.to_game(3),
+                                       font=button_font, bg="#99FF33")
+        self.lowstakes_button.grid(row=0, column=0, pady=10)
+
+    def to_game(self, stakes):
+        starting_balance = self.start_amount_entry.get()
+        Game(self, stakes, starting_balance)
+
+
+class Game:
+    def __init__(self, partner, stakes, starting_balance):
+        print(stakes)
+        print(starting_balance)
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Mystery Box Game")
+    something = Start(root)
+    root.mainloop()
